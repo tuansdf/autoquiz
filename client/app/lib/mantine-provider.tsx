fc8563cfcc6379ea49584ae07380a9e2dab54aa9
tuansdf <103062682+tuansdf@users.noreply.github.com>
2025-08-04 15:05:58 +0700

    chore(client): update colors

--- client/app/lib/mantine-provider.tsx
+++ client/app/lib/mantine-provider.tsx
@@ -2,7 +2,9 @@ import { createTheme, MantineProvider as AMantineProvider } from "@mantine/core"
 import { Notifications } from "@mantine/notifications";
 import type { PropsWithChildren } from "react";
 
-const theme = createTheme({});
+const theme = createTheme({
+  black: "#171717",
+});
 
 export const MantineProvider = (props: PropsWithChildren) => {
   return (
