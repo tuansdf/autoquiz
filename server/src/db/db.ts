f05608108625fc62b3ea28351eff1ef9bb8e0ebf
tuansdf <103062682+tuansdf@users.noreply.github.com>
2025-10-15 15:56:17 +0700

    feat(server): create db pool

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,6 +1,16 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import { Env } from "../env";
 
-export const db = drizzle(Env.DB_URL);
+const pool = new Pool({
+  connectionString: Env.DB_URL,
+  min: 2,
+  max: 20,
+  idleTimeoutMillis: 20_000,
+  connectionTimeoutMillis: 7_000,
+  allowExitOnIdle: false,
+  keepAlive: true,
+});
+export const db = drizzle({ client: pool });
 
 await db.execute("select 1");
