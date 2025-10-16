fbf8e15f93eda22969656cd59411037c3971cf98
tuansdf <103062682+tuansdf@users.noreply.github.com>
2025-10-16 11:07:38 +0700

    chore(client): add react compiler

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,13 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), viteLegacyPlugin(), react()],
+  plugins: [
+    tsconfigPaths(),
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    viteLegacyPlugin(),
+  ],
 });
